b720edfe0ca36b625d76f6b8087ea553ede3837a
Itzrealathul <mahaethyathul@gmail.com>
2025-06-12 10:07:51 +0800

    Add files via upload

---
+++ Hw 2.py
@@ -0,0 +1,15 @@
+
+athul_birthday = '04-12-2014'
+naman_birthday = '26-06-2014'
+rishaan_birthday = '14-05-2014'
+evan_birthday = '09-01-2014'
+sai_birthday= '07-12-2013'
+
+print(athul_birthday)
+print(naman_birthday)
+print(rishaan_birthday)
+print(evan_birthday)
+print(sai_birthday)
+
+
+
