d924b8d39dd9f52f0c7dc7f97b39a340a5534c0c
Itzrealathul <mahaethyathul@gmail.com>
2025-06-12 10:22:24 +0800

    Add files via upload

---
+++ Storing_birthdays.py
@@ -0,0 +1,14 @@
+
+athul_birthday = '04-12-2014'
+naman_birthday = '26-06-2014'
+rishaan_birthday = '14-05-2014'
+evan_birthday = '09-01-2014'
+sai_birthday= '07-12-2013'
+
+print("Athul Birthday is on:",athul_birthday)
+print("Naman Birthday is on:",naman_birthday)
+print("Rishaan Birthday is on:",rishaan_birthday)
+print("Evan Birthday is on:",evan_birthday)
+print("Sai Birthday is on:",sai_birthday)
+
+
