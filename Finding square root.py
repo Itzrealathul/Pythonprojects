b8fd2f36f803431e4852443f28d50b97f3a305a5
Itzrealathul <mahaethyathul@gmail.com>
2025-06-18 09:25:48 +0800

    Add files via upload

---
+++ Finding square root.py
@@ -0,0 +1,7 @@
+import math
+# This program calculates the square root of a given number
+number = float(input("Enter a number to find its square root: "))
+# Calculate the square root using the math module
+square_root = math.sqrt(number)
+# Print the result
+print("The square root of", number, "is", square_root)
