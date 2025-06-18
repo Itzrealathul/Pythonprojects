e7c9d717cc67b64fed4c1f39e1629d57c9c03ae0
Itzrealathul <mahaethyathul@gmail.com>
2025-06-18 10:46:17 +0800

    Add files via upload

---
+++ Greater or less than 15.py
@@ -0,0 +1,8 @@
+number=float(int(input("Enter a number to find if it is greater or lesser than 15: ")))
+if (number >15):
+    print("The number is greater than 15")
+elif (number==15):
+    print("The number is equal to 15")
+    
+else :
+    print("The number is lesser to 15")
