530ac5262dcea4f133c754545a432d35d85cf012
Itzrealathul <mahaethyathul@gmail.com>
2025-06-19 11:29:45 +0800

    Add files via upload

---
+++ Season finder.py
@@ -0,0 +1,7 @@
+temperature=float(int(input("Enter a number to find if it summer or winter: ")))
+if temperature > 30:
+    print("It's summer")
+elif temperature < 5:
+    print("It's winter")
+else:
+    print("It's spring or autumn")
