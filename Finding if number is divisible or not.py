a32b762e83119ffb3bbad0098503acf0ca49f4af
Itzrealathul <mahaethyathul@gmail.com>
2025-06-25 10:44:48 +0800

    Add files via upload

---
+++ Finding if number is divisible or not.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter the 2nd number:"))
+if (a==0 or b==0):
+    print("A number cannot be divided by 0")
+else:           
+    if (a%b==0):
+        print("The number is divisible by 2nd number")
+    
+    else:
+        print("The number is not divisible by 2nd number")
+
