4b7d3f009db9489d00d9eac6da02216c5a009884
Itzrealathul <mahaethyathul@gmail.com>
2025-06-26 12:02:28 +0800

    Add files via upload

---
+++ swaping number values.py
@@ -0,0 +1,9 @@
+number1=input("Enter 1st  number to be swapped:")
+number2=input("Enter 2nd  number to be swapped:")
+number3=input("Enter 3rd  number to be swapped:")
+number=number2
+number2=number3
+number3=number1
+print("After swapping value of first number=",number)
+print("After swapping value of second=",number2)
+print("After swapping value of third=",number3)
