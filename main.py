96ae64e8c378469c11b8e37ce57cb51e90e0ce93
Itzrealathul <mahaethyathul@gmail.com>
2025-06-26 14:12:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-attendance=input("Enter your attedance of class:")
-medical_cause=input("Did you have meadical cause?")
+attendance=int(input("Enter your attedance of class:"))
+
 if attendance>=75:
     print("You are allowed to attend the exam")
 
 else:
+    medical_cause=input("Did you have meadical cause?:")
     if(medical_cause=='y'or medical_cause=='Y'):
         print("You are allowed to attend the exam")
     
+    elif(medical_cause=='N'or medical_cause=='n'):
+        print("You are not allowed to attend the exam")
+    
     else:
-        print("You are not allowed")
+        print("INVALID INPUT")
+
