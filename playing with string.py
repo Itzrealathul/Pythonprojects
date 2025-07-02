0c977dd418bd7183fc95b72a76f0fc558f477966
Itzrealathul <mahaethyathul@gmail.com>
2025-07-02 10:43:46 +0800

    Add files via upload

---
+++ playing with string.py
@@ -0,0 +1,50 @@
+s=input("Enter a string:")
+s2=""
+for i in s:
+    s2=i+s2
+print("The reverse string is",s2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
