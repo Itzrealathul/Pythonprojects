0343f10dab23d4124fd91da5cab8c2ea047498d6
Itzrealathul <mahaethyathul@gmail.com>
2025-07-08 10:49:20 +0800

    Add files via upload

--- Prime numbers.py
+++ Prime numbers.py
@@ -5,5 +5,5 @@ for i in range (lower,upper+1):
         for j in range(2,i):
             if (i%j==0):
                 break
-            else:
-                print(j)
+        else:
+            print(i)
