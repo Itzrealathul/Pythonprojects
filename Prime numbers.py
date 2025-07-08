f03cae15218f2e509e1b0b79a325ce867609e778
Itzrealathul <mahaethyathul@gmail.com>
2025-07-08 10:53:25 +0800

    Update Prime numbers.py

--- Prime numbers.py
+++ Prime numbers.py
@@ -1,5 +1,5 @@
-lower=int(input("Enter the upper range:"))
-upper=int(input("Enter the lower range:"))
+lower=int(input("Enter the lower range:"))
+upper=int(input("Enter the upper range:"))
 for i in range (lower,upper+1):
     if i>1:
         for j in range(2,i):
