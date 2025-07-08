7546cf4740a3f576a08a97cb2d101f3159f0e61f
Itzrealathul <mahaethyathul@gmail.com>
2025-07-08 09:07:57 +0800

    Add files via upload

--- Number of digits.py
+++ Number of digits.py
@@ -1,9 +1,7 @@
-n=int(input("Enter a integer:")
+n=int(input("Enter a integer:"))
 count=1
-while n<10:
+while n>=10:
    n=n//10
    count=count+1
-print("he number of digits in the number is",count)
-
-
+print("The numb
     
