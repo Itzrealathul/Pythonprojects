75c7a2506bd278860586aaea8eb12afc77fd4a8a
Itzrealathul <mahaethyathul@gmail.com>
2025-07-12 12:10:59 +0800

    Add files via upload

---
+++ star.py
@@ -0,0 +1,32 @@
+import turtle
+
+turtle.Screen().bgcolor("grey")
+board = turtle. Turtle()
+
+
+board.forward(100) 
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+bord.pendown()
+board.right(90)
+
+
+board.forward(100) 
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+turtle.hideturtle()
+turtle.done()
