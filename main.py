b81645cc2c27a03a53392e361aa9d08bb67370d0
Itzrealathul <mahaethyathul@gmail.com>
2025-08-16 12:07:14 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-age = int(input("Enter your age: "))
+import array as arr
+array_num = arr.array('i', [1, 3, 7, 1, 7, 3, 9])
+print("The array is",array_num)
+print("The number of times '3' occurs ",array_num.count(3))
+array_num.reverse()
+print("The reverse of array is",str(array_num))
 
-if age >= 10:
-    if age <= 20:
-        print("Age is between 10 and 20")
-    else:
-        print("Age is greater than 20")
-else:
-    print("Age is less than 10")
