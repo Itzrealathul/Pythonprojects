3e9cf65817344d3d1c21b1552c6e66320e99b22c
Itzrealathul <mahaethyathul@gmail.com>
2025-08-20 19:32:54 +0800

    Add files via upload

---
+++ Math function.py
@@ -0,0 +1,10 @@
+numbers1 = [2, 4, 6]
+numbers2 = [3, 5, 7]
+result = map(lambda x, y:x + y,numbers1,numbers2)
+print("Addition of two lists")
+print(list(result))
+nums=[1,2,3,4,5]
+def square(n):
+    return n*n
+s=list(map(square,nums))
+print(square)
