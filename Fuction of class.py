3b061b261b5f61f38a58518a5869b969e225e611
Itzrealathul <mahaethyathul@gmail.com>
2025-08-30 12:13:55 +0800

    Add files via upload

---
+++ Fuction of class.py
@@ -0,0 +1,4 @@
+class student:
+    grade=6
+    print("I study in grade",grade)
+ob=student()
---
+++ class vehicle.py
@@ -0,0 +1,7 @@
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.maxspeed=max_speed 
+        self.mileage=mileage
+modelx=Vehicle(240,80)
+print("Model max speed=",modelx.maxspeed)
+print("Model milage is",modelx.mileage)
